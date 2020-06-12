7a2dfcec961ca0bbfac88a311df8f7d3ac463c9b
Albert Obwoge <albertnyambegaa@gmail.com>
2020-06-12 22:06:47 +0300

    initial page

--- Quotes/src/app/app.component.ts
+++ Quotes/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quote ='The pessimist sees diffuculty in every opportunity'
 }
