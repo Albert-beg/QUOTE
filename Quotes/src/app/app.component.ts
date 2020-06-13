c31a6302b437647d4554c97cc88baa35d562898c
Albert Obwoge <albertnyambegaa@gmail.com>
2020-06-13 21:13:25 +0300

    updates to html

--- Quotes/src/app/app.component.ts
+++ Quotes/src/app/app.component.ts
@@ -8,8 +8,8 @@ import {Quote} from './quote';
 })
 export class AppComponent {
   quotes:Quote[]=[
-    {id:1, name:'The pessimist sees difficuly in every opportunity'},
-    {id:2, name:'Dont let yesterday take up too much today'},
-    {id:3, name:'You learn more from failure than from success'},
+    
+   
+  
   ];
 }
