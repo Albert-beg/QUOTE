ded602b316458e9be21c0f703a8fb2dbf4432c61
Albert Obwoge <albertnyambegaa@gmail.com>
2020-06-14 23:12:33 +0300

    add pipes

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { DetailComponent } from './detail/detail.component';
 import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     DetailComponent,
-    StrikethroughDirective
+    StrikethroughDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
